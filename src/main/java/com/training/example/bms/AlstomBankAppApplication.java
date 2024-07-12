5972eddc58f30693b3a2358743354abc0838afff
Ojas Sangra <osangra@dhcp-10-120-189-252.wifi.oracle.com>
2024-07-12 09:46:48 +0530

    first commit

---
+++ src/main/java/com/training/example/bms/AlstomBankAppApplication.java
@@ -0,0 +1,14 @@
+package com.training.example.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlstomBankAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlstomBankAppApplication.class, args);
+		System.out.println("alstom app created");
+	}
+
+}
---
+++ src/test/java/com/training/example/bms/AlstomBankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.example.bms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlstomBankAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
